cea233d61c6c812a3c553db8cb525bad9befe0eb
frederick <luyunfei0014@gmail.com>
2019-04-06 10:07:25 +0800

    groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,11 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @groups = Group.find(params[id:])
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
   end
   
   def new
@@ -22,8 +26,4 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:title, :description)
   end
-
-
-
-
 end
