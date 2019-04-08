17ee11dc73cd70c68eef609dbb270204a49068ec
frederick <luyunfei0014@gmail.com>
2019-04-08 21:44:15 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,13 +24,10 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+  end
 
 
   def update
-    @group = Group.find(params[:id])
-
-
     if  @group.update(group_params)
        redirect_to groups_path, notice: "Update Success"
     else
@@ -39,7 +36,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
 
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
@@ -60,4 +56,5 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:title, :description)
   end
-end
+
+  end
